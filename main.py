ec3a997c4751aaa4f342e96030b7185ef46bc196
Pedro Sarmiento <pedrosarmientoyanez@gmail.com>
2024-02-23 14:04:27 +0000

    modified:   main.py

--- main.py
+++ main.py
@@ -4,14 +4,24 @@ import streamlit as st
 
 with st.sidebar:
     st.title('🤖💬 SF OpenAI Chatbot')
+    st.sidebar.info("Este chatbot utiliza el modelo de lenguaje GPT-3.5 de OpenAI para responder a tus preguntas. ¡Pruébalo!")
+    
     openai.api_key = st.text_input('Intruduce tu API key de OpenAI', type='password')
     if not (openai.api_key.startswith('sk-') and len(openai.api_key)==51):
         st.warning('Introduce tus credenciales', icon='⚠️')
     else:
         st.success('Puedes proceder a chatear', icon='👉')
+                
+    system_message = st.text_area(label='Mensaje de sistema:',
+                                height=180,
+                                placeholder='Instrucciones que complementan el comportamiento de tu modelo de fine-tuning. Ej: Responde siempre alegre.')
+    
     st.session_state["openai_model"] = st.radio("Selecciona el modleo que deseas usar:", ("gpt-3.5-turbo", "gpt-3.5-turbo FINE_TUNED"))
+    
 if "messages" not in st.session_state:
-    st.session_state.messages = []
+    st.session_state.messages = [
+        {"role": "system", "content": system_message},
+        ]
 
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
@@ -19,7 +29,7 @@ for message in st.session_state.messages:
 
 def clear_chat_history():
     st.session_state.messages = st.session_state.messages = [
-        {"role": "system", "content": prompt},
+        {"role": "system", "content": system_message},
         ]
 st.sidebar.button('Limpiar chat', on_click=clear_chat_history)
       
@@ -40,4 +50,4 @@ if prompt := st.chat_input("Escribe aquí..."):
             ): full_response += str(response.choices[0].delta.content)
         message_placeholder.markdown(full_response[:-4] + "▌")
         message_placeholder.markdown(full_response[:-4])
-    st.session_state.messages.append({"role": "assistant", "content": full_response})
+    st.session_state.messages.append({"role": "assistant", "content": full_response[:-4]})
