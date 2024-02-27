b8fa9de26e066744d32d50ea581125397b56f9c2
Pedro Sarmiento <pedrosarmientoyanez@gmail.com>
2024-02-27 13:28:28 +0000

    modified:   main.py 	deleted:    main2.py 	modified:   requirements.txt

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 import openai
 import streamlit as st
-import base64
-from audio_recorder_streamlit import audio_recorder
-import os
 
-# Initialize Streamlit app
-st.title('🤖💬 SF OpenAI Chatbot')
-
-# Sidebar content
+def clear_chat_history():
+    st.session_state.messages = st.session_state.messages = [
+        {"role": "system", "content": system_message},
+        ]
+    
 with st.sidebar:
     st.title('🤖💬 SF OpenAI Chatbot')
     st.sidebar.info("Este chatbot utiliza el modelo de lenguaje GPT-3.5 de OpenAI para responder a tus preguntas. ¡Pruébalo!")
     
-    openai.api_key = st.text_input('Introduce tu API key de OpenAI', type='password')
+    openai.api_key = st.text_input('Intruduce tu API key de OpenAI', type='password')
     if not (openai.api_key.startswith('sk-') and len(openai.api_key)==51):
         st.warning('Introduce tus credenciales', icon='⚠️')
     else:
@@ -22,85 +20,41 @@ with st.sidebar:
                                 height=180,
                                 placeholder='Instrucciones que complementan el comportamiento de tu modelo de fine-tuning. Ej: Responde siempre alegre.')
     
-    st.session_state["openai_model"] = st.radio("Selecciona el modelo que deseas usar:", ("gpt-3.5-turbo", "gpt-3.5-turbo FINE_TUNED"))
+    st.session_state["openai_model"] = st.radio("Selecciona el modleo que deseas usar:", ("gpt-3.5-turbo", "gpt-3.5-turbo FINE_TUNED"))
+    st.sidebar.button('Limpiar chat', on_click=clear_chat_history)
     
+
+
 if "messages" not in st.session_state:
     st.session_state.messages = [
         {"role": "system", "content": system_message},
-    ]
+        ]
 
-# Function to clear chat history
-def clear_chat_history():
-    st.session_state.messages = [
-        {"role": "system", "content": system_message},
-    ]
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
 
-# Function to transcribe speech to text
-def speech_to_text(audio_data):
-    with open(audio_data, "rb") as audio_file:
-        transcript = openai.Audio.create_transcription(
-            audio_file,
-            model="whisper-1"
-        )
-    return transcript
+if prompt := st.chat_input("Escribe aquí..."):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+        if st.session_state["openai_model"] == "gpt-3.5-turbo FINE_TUNED":
+            model_name = "ft:gpt-3.5-turbo-0613:xrealityfactory:streamlit-test:8wrJg3Ol"
+        else:
+            model_name = st.session_state["openai_model"]
+        for response in openai.chat.completions.create(
+            model=model_name,
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True
+        ):
+            full_response += str(response.choices[0].delta.content)
+        message_placeholder.markdown(full_response[:-4] + "▌")
+        message_placeholder.markdown(full_response[:-4])
+    st.session_state.messages.append({"role": "assistant", "content": full_response[:-4]})
 
-# Function to convert text to speech
-def text_to_speech(input_text):
-    response = openai.Audio.create(
-        input=input_text,
-        model="tts-1",
-        voice="nova"
-    )
-    webm_file_path = "temp_audio_play.mp3"
-    with open(webm_file_path, "wb") as f:
-        response.stream_to_file(webm_file_path)
-    return webm_file_path
-
-# Function to autoplay audio
-def autoplay_audio(file_path: str):
-    with open(file_path, "rb") as f:
-        data = f.read()
-    b64 = base64.b64encode(data).decode("utf-8")
-    md = f"""
-    <audio autoplay>
-    <source src="data:audio/mp3;base64,{b64}" type="audio/mp3">
-    </audio>
-    """
-    st.markdown(md, unsafe_allow_html=True)
-
-# Button to clear chat history
-st.sidebar.button('Limpiar chat', on_click=clear_chat_history)
-
-# Container for the microphone
-footer_container = st.container()
-with footer_container:
-    audio_bytes = audio_recorder()
-
-# Handle audio input
-if audio_bytes:
-    # Transcribe audio
-    with st.spinner("Transcribing..."):
-        transcript = speech_to_text(audio_bytes)
-        st.session_state.messages.append({"role": "user", "content": transcript})
-        st.write("Transcript:", transcript)
-
-# Chat input field
-if prompt := st.text_input("Escribe aquí o graba tu pregunta...", key="user_input"):
-    if prompt.strip() != "":
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        with st.chat_message("user"):
-            st.markdown(prompt)
-        
-        with st.chat_message("assistant"):
-            message_placeholder = st.empty()
-            full_response = ""
-            for response in openai.ChatCompletion.create(
-                model = st.session_state["openai_model"],
-                messages=[
-                    {"role": m["role"], "content": m["content"]}
-                    for m in st.session_state.messages
-                ]
-            ): full_response += str(response.choices[0].delta.content)
-            message_placeholder.markdown(full_response[:-4] + "▌")
-            message_placeholder.markdown(full_response[:-4])
-        st.session_state.messages.append({"role": "assistant", "content": full_response[:-4]})
